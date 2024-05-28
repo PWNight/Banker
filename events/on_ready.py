40e77e11d8a69962b1628f3e71c7969a757c4cba
Найт <rodionvarzymov@outlook.com>
2024-05-28 12:47:48 +0300

    Обновление статуса бота

--- events/on_ready.py
+++ events/on_ready.py
@@ -9,7 +9,7 @@ class OnReady(commands.Cog):
         pass
     @commands.Cog.listener()
     async def on_ready(self):
-        await self.client.change_presence(status=discord.Status.dnd,activity = discord.Activity(type = discord.ActivityType.watching, name = f"за тестированием"))
+        await self.client.change_presence(activity = discord.Activity(type = discord.ActivityType.watching, name = f"за валютой"))
          
 def setup(client):
     client.add_cog(OnReady(client))
