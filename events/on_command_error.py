4262c4d5026c47a8c517bf36aa59cec704bd6324
Найт <rodionvarzymov@outlook.com>
2024-06-04 11:01:47 +0300

    Хотфикс 1.4.1

--- events/on_command_error.py
+++ events/on_command_error.py
@@ -22,10 +22,10 @@ class Error(commands.Cog):
             responce_embed.description = f"Отдел разработки уже получил уведомление о неправильной работе команды и в ближайшее время восстановит работу команды."
             responce_embed.set_footer(text=f'{main.copyright()}',icon_url=f'https://cdn.discordapp.com/attachments/1053188377651970098/1238899111948976189/9.png?ex=6640f635&is=663fa4b5&hm=541eea40573fd92a3861ed259706dff887d9934650b5aab7f698c0e9842cf9bd&')
             responce_embed.timestamp = datetime.now()
-            await inter.send(embed = responce_embed ,ephemeral = True)
+            await inter.send(embed = responce_embed, ephemeral = True)
 
             system_embed = discord.Embed(title=f'Критическая ошибка при выполнении команды /{inter.data.name}', color=0xE36565)
-            system_embed.description = f"\n\nСообщение ошибки: ```{traceback.format_exception(exception)}```"
+            system_embed.description = f"\n\nСообщение ошибки: {traceback.format_exception(exception)}"
             system_embed.set_footer(text=f'{main.copyright()}',icon_url=f'https://cdn.discordapp.com/attachments/1053188377651970098/1238899111948976189/9.png?ex=6640f635&is=663fa4b5&hm=541eea40573fd92a3861ed259706dff887d9934650b5aab7f698c0e9842cf9bd&')
             system_embed.timestamp = datetime.now()
             await logschannel.send(embed = system_embed)
