ecee2cb186311f448e89cacac3177de3a757ced4
PWNight <rodionvarzymov@outlook.com>
2024-07-21 20:33:30 +0300

    Переход на ветку разработки ALPHA-2.0.0

--- configs/config.py
+++ configs/config.py
@@ -1,7 +1,7 @@
 # Основные настройки
 
 token = 'MTE5NTMxNTk4NTUzMjYwNDUwNg.GT-Al-.dG47plP5cVs9GZY4FY-1suQ_h25my9y2Qx-bH0' # Токен бота
-version = '1.7.0' # Версия бота
+version = 'ALPHA-2.0.0' # Версия бота
 prefix = 'b.' # Префикс по стандарту
 
 #Эмодзи
