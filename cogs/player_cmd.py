81a8785f7ac7ea5273b43c653c5d439373c87fd0
PWNight <rodionvarzymov@outlook.com>
2026-01-29 11:22:17 +0300

    Рефакторинг команды /перевести

--- cogs/player_cmd.py
+++ cogs/player_cmd.py
@@ -1,27 +1,31 @@
 import disnake as discord
-import datetime
-from datetime import timezone, timedelta
 from disnake.ext import commands
 from api import main, base
+from api.main import get_timestamp
 from configs import config
 
 class PlayerCMD(commands.Cog):
     def __init__(self, client):
         self.client = client
-    @commands.slash_command(name="перевести", description="💵 Переводит алмазы на указанную карту", guild_ids=[921483461016031263], test_guilds=[921483461016031263])
+        self.logchannel = self.client.get_channel(config.logschannel)
+    @commands.slash_command(name="перевести",
+                            description="💵 Переводит алмазы на указанную карту",
+                            guild_ids=[921483461016031263],
+                            test_guilds=[921483461016031263]
+    )
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def give_money(self, inter, card_id: str, sum: int):
         await inter.response.defer(ephemeral = True)
-        #sum validation
-        if(sum < 0 or sum == 0):
+        # Проверка суммы
+        if sum < 0 or sum == 0:
             await inter.send(f'{config.deny} Введена некорректная сумма. Принимаются только положительные числа.',ephemeral=True)
             return
-        if(sum > 5000):
+        if sum > 5000:
             await inter.send(f'{config.deny} За раз можно перевести не более 5000 алмазов.',ephemeral=True)
             return
         
-        #card id validation
-        if(len(card_id) != 4):
+        # Проверка номера карты
+        if len(card_id) != 4:
             await inter.send(f'{config.deny} Неправильный номер карты. Пример номера: `0001`.',ephemeral=True)
             return
         try:
@@ -29,62 +33,79 @@ class PlayerCMD(commands.Cog):
         except ValueError:
             await inter.send(f'{config.deny} Неправильный номер карты. Пример номера: `0001`.',ephemeral=True)
             return
+
         card_id = int(card_id)
         
-        #get cards info by inter id and card id
+        # Получаем инфу о карте по айди пользователя и карты
         owner_card_info = base.request_one(f"SELECT * FROM `cards` WHERE owner_id = {inter.author.id}")
         reciever_card_info = base.request_one(f"SELECT * FROM `cards` WHERE id = {card_id}")
-        if owner_card_info == None:
+
+        if owner_card_info is None:
             await inter.send(f'{config.deny} На ваше имя нету зарегистрированных карт, обратитесь в отделение банка для оформления карты.',ephemeral=True)
             return
-        if reciever_card_info == None:
+        if reciever_card_info is None:
             await inter.send(f'{config.deny} Карта `FW-{card_id}` не найдена. Убедитесь, что вы ввели правильный номер.',ephemeral=True)
             return
         
-        #check users id
+        # Сверяем айди пользователей
         owner_id = owner_card_info['owner_id']
         reciever_id = reciever_card_info['owner_id']
-        if(owner_id == reciever_id):
+        if owner_id == reciever_id:
             await inter.send(f'{config.deny} Вы не можете перевести алмазы самому себе.',ephemeral=True)
             return
-        
-        logchannel = self.client.get_channel(config.logschannel)
+
         owner = await self.client.fetch_user(int(owner_id))
         owner_card_id = owner_card_info['id']
         reciever = await self.client.fetch_user(int(reciever_id))
-        timezone_offset = +3.0
-        tzinfo = timezone(timedelta(hours=timezone_offset))
-        date = str(datetime.datetime.now(tzinfo)).split('.')[0]
-        date_format = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
-        timestamp = int(str(datetime.datetime.timestamp(date_format)).split('.')[0])
-        timestamp = f"<t:{timestamp}:f>"
 
-        #get and calc new balance
+        timestamp = get_timestamp()
+
+        # Получаем баланс и расчитываем новый
         owner_balance = int(owner_card_info['balance'])
         reciever_balance = int(reciever_card_info['balance'])
+
         if owner_balance < sum:
             await inter.send(f'{config.deny} На карте `FW-{owner_card_id}` недостаточно средств (Баланс: `{owner_balance}` алмазов, а снимается `{sum}` алмазов).',ephemeral=True)
             return
+
         owner_balance -= sum
         reciever_balance += sum
         
-        #update balance in DB
+        # Обновляем баланс в БД
         base.send(f"UPDATE `cards` SET `balance` = {owner_balance} WHERE id = {owner_card_id}")
         base.send(f"UPDATE `cards` SET `balance` = {reciever_balance} WHERE id = {card_id}")
 
-        #gen and send responce
+        # Отправляем ответ
         await inter.send(f"{config.accept} 💸 Вы перевели {sum} алмазов на карту `FW-{card_id}`.",ephemeral=True)
 
-        responce_chnl_system = discord.Embed(description=f"### 💸 Пользователь {owner.mention} перевёл пользователю {reciever.mention} {sum} алмазов \nКарта владельца: `FW-{owner_card_id}`. \nКарта получателя: `FW-{card_id}`. \n\nДата оформления транзакции: {timestamp}.",color=0xEFAF6F)
-        responce_chnl_system.set_footer(text=f'{main.copyright()}', icon_url=f'https://cdn.discordapp.com/attachments/1053188377651970098/1238899111948976189/9.png?ex=6640f635&is=663fa4b5&hm=541eea40573fd92a3861ed259706dff887d9934650b5aab7f698c0e9842cf9bd&')
-        await logchannel.send(embed=responce_chnl_system)
+        responce_chnl_system = discord.Embed(
+            description=f"### 💸 Пользователь {owner.mention} перевёл пользователю {reciever.mention} {sum} алмазов \nКарта владельца: `FW-{owner_card_id}`. \nКарта получателя: `FW-{card_id}`. \n\nДата оформления транзакции: {timestamp}.",
+            color=0xEFAF6F
+        )
+        responce_chnl_system.set_footer(
+            text=f'{main.copyright()}',
+            icon_url=f'https://cdn.discordapp.com/attachments/1053188377651970098/1238899111948976189/9.png?ex=6640f635&is=663fa4b5&hm=541eea40573fd92a3861ed259706dff887d9934650b5aab7f698c0e9842cf9bd&'
+        )
+        await self.logchannel.send(embed=responce_chnl_system)
 
-        responce_owner_pm = discord.Embed(description=f"### Вы перевели {sum} алмазов на карту `FW-{card_id}` \nДата оформления транзакции: {timestamp}.",color=0xEFAF6F)
-        responce_owner_pm.set_footer(text=f'{main.copyright()}', icon_url=f'https://cdn.discordapp.com/attachments/1053188377651970098/1238899111948976189/9.png?ex=6640f635&is=663fa4b5&hm=541eea40573fd92a3861ed259706dff887d9934650b5aab7f698c0e9842cf9bd&')
+        responce_owner_pm = discord.Embed(
+            description=f"### Вы перевели {sum} алмазов на карту `FW-{card_id}` \nДата оформления транзакции: {timestamp}.",
+            color=0xEFAF6F
+        )
+        responce_owner_pm.set_footer(
+            text=f'{main.copyright()}',
+            icon_url=f'https://cdn.discordapp.com/attachments/1053188377651970098/1238899111948976189/9.png?ex=6640f635&is=663fa4b5&hm=541eea40573fd92a3861ed259706dff887d9934650b5aab7f698c0e9842cf9bd&'
+        )
         await owner.send(embed=responce_owner_pm)
 
-        responce_reciever_pm = discord.Embed(description=f"### Вы получили {sum} алмазов на карту `FW-{card_id}` \nПеревод поступил от {owner.mention} (`FW-{owner_card_id}`) \nДата оформления транзакции: {timestamp}.",color=0xEFAF6F)
-        responce_reciever_pm.set_footer(text=f'{main.copyright()}', icon_url=f'https://cdn.discordapp.com/attachments/1053188377651970098/1238899111948976189/9.png?ex=6640f635&is=663fa4b5&hm=541eea40573fd92a3861ed259706dff887d9934650b5aab7f698c0e9842cf9bd&')
+        responce_reciever_pm = discord.Embed(
+            description=f"### Вы получили {sum} алмазов на карту `FW-{card_id}` \nПеревод поступил от {owner.mention} (`FW-{owner_card_id}`) \nДата оформления транзакции: {timestamp}.",
+            color=0xEFAF6F
+        )
+        responce_reciever_pm.set_footer(
+            text=f'{main.copyright()}',
+            icon_url=f'https://cdn.discordapp.com/attachments/1053188377651970098/1238899111948976189/9.png?ex=6640f635&is=663fa4b5&hm=541eea40573fd92a3861ed259706dff887d9934650b5aab7f698c0e9842cf9bd&'
+        )
         await reciever.send(embed=responce_reciever_pm)
         
     @commands.slash_command(name="оплатить-счёт", description="💵 Оплачивает указанный счёт", guild_ids=[921483461016031263], test_guilds=[921483461016031263])
@@ -137,18 +158,14 @@ class PlayerCMD(commands.Cog):
         #update balance and invoice status
         base.send(f"UPDATE `cards` SET `balance` = '{owner_balance}' WHERE id = '{owner_card_id}'")
         card1_info = base.request_one(f"SELECT balance FROM `cards` WHERE id = 0001")
+
         balance = int(card1_info['balance'])
         balance += amount
+
         base.send(f"UPDATE `cards` SET `balance` = '{balance}' WHERE id = 0001")
         base.send(f"UPDATE `invoices` SET `status`= 'Оплачен' WHERE id = '{invoice_id}'")
 
-        logchannel = self.client.get_channel(int(config.logschannel))
-        timezone_offset = +3.0
-        tzinfo = timezone(timedelta(hours=timezone_offset))
-        date = str(datetime.datetime.now(tzinfo)).split('.')[0]
-        date_format = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
-        timestamp = int(str(datetime.datetime.timestamp(date_format)).split('.')[0])
-        timestamp = f"<t:{timestamp}:f>"
+        timestamp = get_timestamp()
 
         #remove fine if type == fine
         if(type == 'Штраф'):
@@ -178,7 +195,7 @@ class PlayerCMD(commands.Cog):
                 responce_pm2 = discord.Embed(description=f"### Вас счёт `{invoice_id}` успешно оплачен \nТип счёта: `{type}`\nСумма счёта: `{amount}` алмазов \n\nСчёт оформлен банкиром {invoice_author.mention} \nДата выполнения операции: {timestamp}.",color=0x80d8ed)
             
             responce_chnl_system.set_footer(text=f'{main.copyright()}', icon_url=f'https://cdn.discordapp.com/attachments/1053188377651970098/1238899111948976189/9.png?ex=6640f635&is=663fa4b5&hm=541eea40573fd92a3861ed259706dff887d9934650b5aab7f698c0e9842cf9bd&')
-            await logchannel.send(embed=responce_chnl_system)
+            await self.logchannel.send(embed=responce_chnl_system)
 
             responce_pm2.set_footer(text=f'{main.copyright()}', icon_url=f'https://cdn.discordapp.com/attachments/1053188377651970098/1238899111948976189/9.png?ex=6640f635&is=663fa4b5&hm=541eea40573fd92a3861ed259706dff887d9934650b5aab7f698c0e9842cf9bd&')
             await owner.send(embed=responce_pm2)
