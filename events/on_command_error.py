a9f231bb609646e4f3b1da31b875136e07ddced1
PWNight <rodionvarzymov@outlook.com>
2026-01-29 11:34:23 +0300

    Перенёс изменения сообщения об ошибке из 2.0 ветки

--- events/on_command_error.py
+++ events/on_command_error.py
@@ -5,25 +5,19 @@ from disnake.ext import commands
 from configs import config
 from api import main
 
-
 class Error(commands.Cog):
     def __init__(self, client):
         self.client = client
 
     @commands.Cog.listener()
     async def on_slash_command_error(self,inter, exception):
-        embed = discord.Embed(
-            description=f"<a:load:1256975206455447643> Произошла ошибка при выполнении команды, выясняю причину..",
-            color=0x2f3136
-        )
-        await inter.send(embed = embed, ephemeral = True)
-
+        embed = discord.Embed(color=0x2f3136)
         if isinstance(exception, commands.MissingRole):
             embed.description = f"<:minecraft_deny:1080779495386140684> У вас нету доступа к данной команде."
-            await inter.edit_original_response(embed = embed)
+            await inter.send(embed = embed, ephemeral = True)
         elif isinstance(exception, commands.CommandOnCooldown):
             embed.description = f"<:minecraft_deny:1080779495386140684> Притормозите! Попробуйте использовать команду чуть позже."
-            await inter.edit_original_response(embed = embed)
+            await inter.send(embed = embed, ephemeral = True)
         else:
             logschannel = self.client.get_channel(config.logserrors)
             responce_embed = discord.Embed(
@@ -35,7 +29,7 @@ class Error(commands.Cog):
                 icon_url=f'https://cdn.discordapp.com/attachments/1053188377651970098/1238899111948976189/9.png?ex=6640f635&is=663fa4b5&hm=541eea40573fd92a3861ed259706dff887d9934650b5aab7f698c0e9842cf9bd&'
             )
             responce_embed.timestamp = datetime.now()
-            await inter.edit_original_response(embed = responce_embed)
+            await inter.send(embed = responce_embed, ephemeral = True)
 
             system_embed = discord.Embed(
                 description=f'## Неизвестная ошибка при выполнении команды /{inter.data.name} \nСообщение ошибки: {traceback.format_exception(exception)}',
