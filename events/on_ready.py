67ddc30af221497b91ea67f01f4619e14d4a0794
PWNight <rodionvarzymov@outlook.com>
2026-01-29 10:41:36 +0300

    Убрал лишнюю ф-ию

--- events/on_ready.py
+++ events/on_ready.py
@@ -5,8 +5,6 @@ from disnake.ext import commands
 class OnReady(commands.Cog):
     def __init__(self, client):
         self.client = client
-    async def expired_notify(date):
-        pass
     @commands.Cog.listener()
     async def on_ready(self):
         await self.client.change_presence(activity = discord.Activity(type = discord.ActivityType.watching, name = f"за валютой"))
