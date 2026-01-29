9148c9515eb5d9e0c2f8422ac893c15377cf8695
PWNight <rodionvarzymov@outlook.com>
2026-01-29 11:22:36 +0300

    Перенёс генерацию timestamp в отдельную ф-ию

--- api/main.py
+++ api/main.py
@@ -1,6 +1,15 @@
 import datetime
+from datetime import timezone, timedelta
+
 def year():
     return str(datetime.datetime.now().year)
 
 def copyright():
     return f"© 2023-{year()} FoxWorld Ecosystem"
+
+def get_timestamp():
+    tzinfo = timezone(timedelta(hours=+3))
+    date = str(datetime.datetime.now(tzinfo)).split('.')[0]
+    date_format = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+
+    return f"<t:{int(str(datetime.datetime.timestamp(date_format)).split('.')[0])}:f>"
